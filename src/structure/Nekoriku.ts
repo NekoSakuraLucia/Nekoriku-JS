1817e492282118b189421ba34f75f0a1a7e78c09
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-20 16:41:23 +0700

    Add a class for the actual code and add declarations in the tsconfig.

--- src/structure/Nekoriku.ts
+++ src/structure/Nekoriku.ts
@@ -1,3 +1,26 @@
-export function Nekoriku() {
-    console.log("Nekoriku")
+import { log } from "console";
+import { Client, GatewayIntentBits, Events } from "discord.js";
+
+class Nekoriku {
+    private client: Client;
+
+    constructor() {
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent
+            ]
+        });
+    }
+
+    public start(token: string) {
+        this.client.once(Events.ClientReady, () => {
+            log(`Logged in as ${this.client.user?.tag}`)
+        })
+
+        this.client.login(token);
+    }
 }
+
+export { Nekoriku };
