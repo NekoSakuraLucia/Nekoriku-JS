caa8e64f1abe743b7755c701d02085c6e088e7f0
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-29 12:50:46 +0700

    Change from function to module.exports instead.

--- src/structure/commands/ping.ts
+++ src/structure/commands/ping.ts
@@ -1,9 +1,12 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder()
+const data = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("Replies with Pong!");
 
-export async function execute(interaction: CommandInteraction) {
-  await interaction.reply("Pong!");
-}
+module.exports = {
+  data: data,
+  execute: async (interaction: CommandInteraction<"cached">) => {
+    await interaction.reply("Pong!");
+  }
+};
